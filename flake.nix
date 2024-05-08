19ad11f50381a375031ba28c0c005e9572f4500d
matus.benko <matus.benko@gmail.com>
2024-05-08 02:00:36 +0200

    support playwright 1.43.0
    grabbed from the open pull request
    https://github.com/NixOS/nixpkgs/pull/302759

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Collection tools and utilities used in my development process";
 
   inputs = {
+    nixpkgsPlaywright.url = "github:kalekseev/nixpkgs/playwright-core";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # nixpkgs.url = "/home/primamateria/dev/nixpkgs";
     utils.url = "github:numtide/flake-utils";
@@ -14,6 +15,7 @@
   outputs = inputs @ {
     self,
     nixpkgs,
+    nixpkgsPlaywright,
     utils,
     haumea,
     ...
@@ -24,9 +26,13 @@
           inherit system;
           config = {allowUnfree = true;};
         };
+        pkgsPlaywright = import nixpkgsPlaywright {
+          inherit system;
+          config = {allowUnfree = true;};
+        };
       in (haumea.lib.load {
         src = ./src;
-        inputs = {inherit pkgs;};
+        inputs = {inherit pkgs pkgsPlaywright;};
       })
     );
 }
--- src/profileDefinitions/playwright.nix
+++ src/profileDefinitions/playwright.nix
@@ -1,22 +1,18 @@
 {
-  pkgs,
+  pkgsPlaywright,
   root,
 }: let
   inherit (root) packages;
-
-  playwright-driver = pkgs.playwright-driver.overrideAttrs (finalAttrs: previousAttrs: {
-    version = "1.43.0";
-  });
 in {
   packages = [
-    playwright-driver.browsers
+    pkgsPlaywright.playwright-driver.browsers
     packages.playdebug
   ];
 
   shellHook = ''
     # Prepare playwright
     export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
-    export PLAYWRIGHT_BROWSERS_PATH=${playwright-driver.browsers}
+    export PLAYWRIGHT_BROWSERS_PATH=${pkgsPlaywright.playwright-driver.browsers}
     export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
   '';
 }
