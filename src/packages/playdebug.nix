583043723189b01f46887f8379bd83b31fd4b23d
matus.benko <matus.benko@gmail.com>
2024-05-16 17:04:33 +0200

    playdebug with nodebug option

--- src/packages/playdebug.nix
+++ src/packages/playdebug.nix
@@ -6,6 +6,8 @@
     runtimeInputs = [pkgs.fzf];
     text = ''
       TEMP_FILE="/tmp/testcase.txt"
+      no_debug=false
+      repeat=false
 
       # Function to find testcase and save to temp file
       find_and_save_testcase() {
@@ -23,20 +25,42 @@
           fi
       }
 
-      # Check if the script is called with any arguments
+      # Parse command-line options
+      while getopts ":rx" opt; do
+          case $opt in
+              r)
+                  repeat=true
+                  ;;
+              x)
+                  no_debug=true
+                  ;;
+              \?)
+                  echo "Invalid option: -$OPTARG" >&2
+                  exit 1
+                  ;;
+          esac
+      done
+      shift $((OPTIND - 1))
+
+      # Check if any arguments are left after parsing options
       if [ $# -gt 0 ]; then
-          # Check if the first argument is -r or --repeat
-          if [[ "$1" == "-r" || "$1" == "--repeat" ]]; then
-              repeat_testcase
-          else
-              find_and_save_testcase
-          fi
+          echo "Error: Unexpected argument(s) provided."
+          exit 1
+      fi
+
+      # If repeat is set
+      if $repeat; then
+          repeat_testcase
       else
           find_and_save_testcase
       fi
 
-      # Run the test with the obtained testcase
-      npm run test:int:debug -- "$TESTCASE"
+      # Determine which test command to run based on the presence of the --noDebug flag
+      if $no_debug; then
+          npm run test:int:run -- "$TESTCASE"
+      else
+          npm run test:int:debug -- "$TESTCASE"
+      fi
     '';
   }
 )
