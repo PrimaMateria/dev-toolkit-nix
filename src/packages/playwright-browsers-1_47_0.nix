2ea1ea42b6dd0107ea1bb2c1fb7752db0137e6a2
matus.benko <matus.benko@gmail.com>
2024-09-10 11:16:02 +0200

    playwright 1.47.0

---
+++ src/packages/playwright-browsers-1_47_0.nix
@@ -0,0 +1,7 @@
+{
+  pkgs,
+  super,
+}: (super.playwright-browsers-base {
+  version = "1.47.0";
+  sha256 = "sha256-cKjVDy1wFo8NlF8v+8YBuQUF2OUYjCmv27uhEoVUrno=";
+})
--- src/profileDefinitions/playwright.nix
+++ src/profileDefinitions/playwright.nix
@@ -3,13 +3,13 @@
 in {
   packages = [
     packages.playdebug
-    packages.playwright-browsers-1_46_1
+    packages.playwright-browsers-1_47_0
   ];
 
   shellHook = ''
     # Prepare playwright
     export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
-    export PLAYWRIGHT_BROWSERS_PATH=${packages.playwright-browsers-1_46_1}
+    export PLAYWRIGHT_BROWSERS_PATH=${packages.playwright-browsers-1_47_0}
     export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
   '';
 }
