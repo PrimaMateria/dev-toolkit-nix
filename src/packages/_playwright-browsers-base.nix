2a9e1d9557c61c6354c33af582533a63fd3c295c
matus.benko <matus.benko@gmail.com>
2026-03-20 09:11:32 +0100

    playwright 1.58.2
    the path of headless chromium adjusted

--- src/packages/_playwright-browsers-base.nix
+++ src/packages/_playwright-browsers-base.nix
@@ -5,6 +5,21 @@
 }: let
   fontconfig = pkgs.makeFontsConf {fontDirectories = [];};
 
+  chromiumDir =
+    if pkgs.lib.versionAtLeast version "1.58.0"
+    then "chrome-linux64"
+    else "chrome-linux";
+
+  headlessShellDir =
+    if pkgs.lib.versionAtLeast version "1.58.0"
+    then "chrome-headless-shell-linux64"
+    else "chrome-linux";
+
+  headlessShellBin =
+    if pkgs.lib.versionAtLeast version "1.58.0"
+    then "chrome-headless-shell"
+    else "headless_shell";
+
   playwright-browsers-json = pkgs.stdenv.mkDerivation rec {
     name = "playwright-${version}-browsers";
     src = pkgs.fetchFromGitHub {
@@ -29,21 +44,21 @@ in
   ''
     BROWSERS_JSON=${playwright-browsers-json}/browsers.json
     CHROMIUM_REVISION=$(jq -r '.browsers[] | select(.name == "chromium").revision' $BROWSERS_JSON)
-    mkdir -p $out/chromium-$CHROMIUM_REVISION/chrome-linux
+    mkdir -p $out/chromium-$CHROMIUM_REVISION/${chromiumDir}
 
     # See here for the Chrome options:
     # https://github.com/NixOS/nixpkgs/issues/136207#issuecomment-908637738
-    makeWrapper ${pkgs.chromium}/bin/chromium $out/chromium-$CHROMIUM_REVISION/chrome-linux/chrome \
+    makeWrapper ${pkgs.chromium}/bin/chromium $out/chromium-$CHROMIUM_REVISION/${chromiumDir}/chrome \
       --set SSL_CERT_FILE /etc/ssl/certs/ca-bundle.crt \
       --set FONTCONFIG_FILE ${fontconfig}
 
     # We also need to install the headless shell version of Chromium
     CHROMIUM_HEADLESS_SHELL_REVISION=$(jq -r '.browsers[] | select(.name == "chromium-headless-shell").revision' $BROWSERS_JSON)
-    mkdir -p $out/chromium-headless-shell-$CHROMIUM_HEADLESS_SHELL_REVISION/chrome-linux
+    mkdir -p $out/chromium_headless_shell-$CHROMIUM_HEADLESS_SHELL_REVISION/${headlessShellDir}
 
     # See here for the Chrome options:
     # https://github.com/NixOS/nixpkgs/issues/136207#issuecomment-908637738
-    makeWrapper ${pkgs.chromium}/bin/chromium $out/chromium_headless_shell-$CHROMIUM_REVISION/chrome-linux/headless_shell \
+    makeWrapper ${pkgs.chromium}/bin/chromium $out/chromium_headless_shell-$CHROMIUM_HEADLESS_SHELL_REVISION/${headlessShellDir}/${headlessShellBin} \
        --set SSL_CERT_FILE /etc/ssl/certs/ca-bundle.crt \
        --set FONTCONFIG_FILE ${fontconfig}
 
